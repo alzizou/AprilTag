52df6da0034d2612b1c48347a40957af67b06714
alzizou <ali.safaei@mail.mcgill.ca>
2021-01-04 11:17:14 -0500

    initiate the ROS package

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali/ali_ws/AprilTag/src
+# Build directory: /home/ali/ali_ws/AprilTag/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("apriltag_ali")
--- src/apriltag_ali/src/detect_ali.py
+++ src/apriltag_ali/src/detect_ali.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#import rospy
+import rospy
 import cv2
 import apriltag
 import json
